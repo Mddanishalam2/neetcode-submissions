28b7c55dbb4dc70bec119300b3b1a78c8f6c85d6
Md Danish Alam <rankersacademy0@gmail.com>
2026-07-07 18:07:30 +0530

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,9 @@
+with ranked as(
+    select * ,row_number() over(partition by student_id order by score desc,exam_id asc)
+    as rnk from exam_results
+)
+
+select student_id,exam_id,score
+from ranked
+where rnk=1
+order by student_id;
